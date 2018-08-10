92c0534215df39e561c5ee129c716559d27c7945
designcrumble <y13uc147@lnmiit.ac.in>
2018-08-10 21:01:53 +0530

    landing-page responsive[wip]

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -47,3 +47,43 @@ $(document).ready(function () {
 		});
 
 	});
+
+$.fn.numberChange = function(options) {
+
+  var settings = $.extend({
+    wait: 1000,
+    duration: 1000,
+    startValue: 100,
+    endValue: 0,
+    prefix: '',
+    easing: 'swing'
+  }, options)
+  
+  var that = this;
+  that.html(settings.prefix + settings.startValue);
+  
+  $({number: settings.startValue})
+    .delay(settings.wait)
+    .animate({number:settings.endValue},{
+    duration: settings.duration,
+    easing: settings.easing,
+    step: function(now, tween) {
+      that.html(settings.prefix + Math.floor(now));
+    }
+  });
+
+  return this;
+  
+}
+
+
+$(".number").click(function() {
+  $(this).numberChange({
+    easing: 'easeOutQuint',
+    wait: 0,
+    duration: 4000,
+    prefix:'$',
+    startValue: 575,
+    endValue: 310
+  });
+})
